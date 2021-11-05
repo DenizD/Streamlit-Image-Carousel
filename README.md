46ca98c0c6c58f86fabdec9b8aa4e59cd0baaffb
Deniz <deniz@Denizs-Mac-mini.fritz.box>
2021-11-05 01:37:35 +0100

    image carousel for streamlit

---
+++ README.md
@@ -0,0 +1,14 @@
+### Streamlit Image-Carousel-Component
+
+Streamlit component for image-carousel:
+
+
+
+#### Usage:
+
+- First install the streamlit library : `pip install streamlit`
+- Then, install the node packages inside `frontend` directory : `npm i`
+- Then, build the node modules : `npm run build`
+- Finally, run the streamlit app : `streamlit run __init__.py`
+
+![result](sample_recording.gif)
---
+++ frontend/src/ImageGallery.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import { Streamlit, setStreamlitLifecycle } from "./streamlit";
+  setStreamlitLifecycle();
+
+  export let imageUrls: Array<string>;
+  export let height: number;
+
+  let selectedImageUrl: string;
+
+  function image_onclick(event: any) {
+    selectedImageUrl = event.srcElement.currentSrc;
+    Streamlit.setComponentValue(selectedImageUrl);
+  }
+</script>
+
+
+<div class="scroller">
+  {#each imageUrls as imageUrl}
+      <img src={imageUrl} id={imageUrl} alt="" style="height: {height}px;" on:click={image_onclick}>
+  {/each}
+</div>
+
+
+<style>
+  .scroller {
+    min-height: 100px;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    white-space: nowrap;
+  }
+	img { 
+    display: inline-block;
+    padding: 1%;
+    border-radius: 20px;
+    opacity: 0.9; 
+    transition: all .2s; 
+  }
+
+	img:hover { 
+    opacity: 1; 
+    transform: scale(1.05); 
+    cursor: pointer;
+  }
+</style>
+
+
+
+
+
+
